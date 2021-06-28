0f4bc05a669d70aa4d156d448fd142aa77d513d2
Henio <henioajunior@gmail.com>
2021-06-28 14:40:07 -0300

    0-0-2-cadastro-novo-autor

---
+++ src/main/java/br/com/zupacademy/henio/casadocodigo/novoautor/AutoresController.java
@@ -0,0 +1,28 @@
+package br.com.zupacademy.henio.casadocodigo.novoautor;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceContexts;
+import javax.transaction.Transactional;
+import javax.validation.Valid;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/autores")
+public class AutoresController {
+	
+	@PersistenceContext
+	private EntityManager manager;
+	
+	@PostMapping
+	@Transactional
+	public String criar(@RequestBody @Valid NovoAutorRequest request) {
+		Autor autor = request.toModel();
+		manager.persist(autor);
+		return autor.toString();
+	}	
+}
